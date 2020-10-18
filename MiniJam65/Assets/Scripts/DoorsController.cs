f3d523cf8052209aeadb3b0371ee0eeaa7f4265e
ZactarZero <carlos_gasperin@hotmail.com>
2020-10-18 00:32:56 -0300

    map v1

---
+++ MiniJam65/Assets/Scripts/DoorsController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorsController : MonoBehaviour
+{
+    public OpeningType ot;
+    private Animator animator;
+    private BoxCollider hitBox;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        hitBox = GetComponent <BoxCollider>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            switch (ot)
+            {
+                case OpeningType.Regular:
+                    OpenDoors();
+                    break;
+                case OpeningType.Keycard:
+
+                    break;
+                case OpeningType.Password:
+
+                    break;
+                case OpeningType.EyeScan:
+
+                    break;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            switch (ot)
+            {
+                case OpeningType.Regular:
+                    CloseDoors();
+                    break;
+                case OpeningType.Keycard:
+                    
+                    break;
+                case OpeningType.Password:
+
+                    break;
+                case OpeningType.EyeScan:
+
+                    break;
+            }
+        }
+    }
+
+    private void OpenDoors()
+    {
+        animator.SetTrigger("Open");
+        hitBox.enabled = false;
+    }
+
+    private void CloseDoors()
+    {
+        animator.SetTrigger("Close");
+        hitBox.enabled = true;
+    }
+}
+
+public enum OpeningType
+{
+    Regular,
+    Keycard,
+    Password,
+    EyeScan
+}
---
+++ MiniJam65/Assets/Scripts/VentController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VentController : MonoBehaviour
+{
+    private Animator animator;
+    private BoxCollider hitBox;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        hitBox = GetComponent<BoxCollider>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            //if screwdriver
+            Open();
+        }
+    }
+
+    public void Open()
+    {
+        animator.SetTrigger("Open");
+        hitBox.enabled = false;
+    }
+}
